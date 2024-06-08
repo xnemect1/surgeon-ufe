446bb931a8f1413352d010af868872e966603832
Tomas Nemec <xnemect@gmail.com>
2024-06-08 23:32:51 +0200

    message

--- src/components/xnemect-surgery-editor/test/xnemect-surgery-editor.spec.tsx
+++ src/components/xnemect-surgery-editor/test/xnemect-surgery-editor.spec.tsx
@@ -1,11 +1,11 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { XnemectSurgeryEditor } from '../xnemect-surgery-editor';
 
-describe('xnemect-surgeon-wl-editor', () => {
+describe('xnemect-surgeon-editor', () => {
   it('renders', async () => {
     await newSpecPage({
       components: [XnemectSurgeryEditor],
-      html: `<xnemect-surgeon-wl-editor entry-id="@new"></xnemect-surgeon-wl-editor>`,
+      html: `<xnemect-surgeon-editor entry-id="@new"></xnemect-surgeon-editor>`,
     });
   });
 });
